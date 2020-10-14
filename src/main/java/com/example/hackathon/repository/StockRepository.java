6085ea6973b6797ba2642edd1ed341a7302bfdeb
unknown <ahilan004@gmail.com>
2020-10-14 14:31:54 +0530

    first commit

---
+++ src/main/java/com/example/hackathon/repository/StockRepository.java
@@ -0,0 +1,22 @@
+package com.example.hackathon.repository;
+
+import java.util.Date;
+import java.util.List;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.hackathon.model.Trade;
+
+@Repository
+public interface StockRepository extends MongoRepository<Trade, String> {
+
+	
+	public List<Trade> findByDate(String date);
+	public List<Trade> findByTicker(String ticker);
+	public List<Trade> findByTickerAndUsername(String ticker,String Username);
+	//public List<Stock> findByPriceRange(double price1, double price2);
+ 	
+ 	public Trade findBy_id(ObjectId _id);
+}
